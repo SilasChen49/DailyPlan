f3e550e53b4770975e117d366fb95d22ff3459a8
SilasChen49 <SilasChen49@gmail.com>
2017-09-29 16:10:58 -0400

    d

--- src/main/java/com/cm/web/AccountController.java
+++ src/main/java/com/cm/web/AccountController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.logging.Logger;
 
 @Controller
+@RequestMapping(value = "/index")
 public class AccountController {
 
     @Autowired
